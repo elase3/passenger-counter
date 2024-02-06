edabb0580f563461328f29c054204234ea6be488
elase3 <68402429+elase3@users.noreply.github.com>
2024-02-06 21:49:12 +0200

    Update index.js

--- index.js
+++ index.js
@@ -38,3 +38,23 @@ window.addEventListener("orientationchange", doOnOrientationChange);
 
 // Initial execution if needed
 doOnOrientationChange();
+
+
+const handleSubmit = (event) => {
+  event.preventDefault();
+
+  const myForm = event.target;
+  const formData = new FormData(myForm);
+  
+  fetch("/", {
+    method: "POST",
+    headers: { "Content-Type": "application/x-www-form-urlencoded" },
+    body: new URLSearchParams(formData).toString(),
+  })
+    .then(() => console.log("Form successfully submitted"))
+    .catch((error) => alert(error));
+};
+
+document
+  .querySelector("form")
+  .addEventListener("submit", handleSubmit);
